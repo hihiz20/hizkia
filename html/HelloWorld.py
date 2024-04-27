1cf66326ff7c642cfb6cf5ea928e8872f909cc3f
hihiz20 <163571012+hihiz20@users.noreply.github.com>
2024-04-28 06:02:53 +0900

    Add files via upload

--- html/HelloWorld.py
+++ html/HelloWorld.py
@@ -5,13 +5,38 @@ import string
 class HelloWorld(object):
     @cherrypy.expose
     def index(self):
-        return "Hello world!"
+        return """
+          <html>
+            <head>
+                <link href="/static/css/cherry.css" rel="stylesheet">
+            </head>
+            <body>
+                <form method="get" action="checknumber">
+                    <input type="text" value="13" name="number" />
+                    <button type="submit">Is it prime?</button>
+                </form>
+            </body>
+          </html>
+        """
 
     @cherrypy.expose
     def generate(self):
-        # Generates a random password
         return ''.join(random.sample(string.hexdigits, 8))
 
+    @cherrypy.expose
+    def checknumber(self, number=13):
+        try:
+            number = int(number)
+            if number < 2:
+                return f"{number} is not prime (less than 2)."
+            for i in range(2, int(number ** 0.5) + 1):
+                if (number % i) == 0:
+                    return f"{number} is not prime."
+            return f"{number} is prime."
+        except ValueError:
+            return "Error: Invalid input. Please provide a valid integer."
+
 if __name__ == '__main__':
-    cherrypy.server.socket_host = '0.0.0.0'  # Listen on all network interfaces
+    cherrypy.config.update({'/static': {'tools.staticdir.on': True,
+                                        'tools.staticdir.dir': '/path/to/your/static'}})
     cherrypy.quickstart(HelloWorld())
